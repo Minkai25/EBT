b5dd5c0c74af28dda8102ff0e7521167caf146b3
Minkai Li <minli@holygpu7c26105.rc.fas.harvard.edu>
2025-10-07 21:36:32 -0400

    Add train dataset of arc

--- base_model_trainer.py
+++ base_model_trainer.py
@@ -26,6 +26,7 @@ from data.nlp.squad_dataloader import SQuADDataset
 from data.nlp.ai2arc_dataloader import AI2ArcDataset
 from data.nlp.planbench_dataloader import PlanBenchDataset
 from data.nlp.synthetic_dataset import NLPSyntheticDataset
+from data.arc.original_arc_dataset import ARCTrainDataset
 
 from model.vid.ebt import EBT_VID
 from model.nlp.ebt import EBT_NLP
@@ -537,6 +538,9 @@ class ModelTrainer(L.LightningModule):
             elif self.hparams.dataset_name == "squad":
                 self.train_ds = SQuADDataset(self.hparams, split = 'train')
                 self.val_ds = SQuADDataset(self.hparams, split = 'validation')
+            elif self.hparams.dataset_name == "arc":
+                self.train_ds = ARCTrainDataset()
+                self.val_ds = ARCTrainDataset() # no val split so just use train split for now
             else:
                 raise NotImplementedError("Haven't implemented this dataset yet")
             print(f"{self.hparams.dataset_name} length of train_dataset: {len(self.train_ds)} and val_dataset: {len(self.val_ds)}")
